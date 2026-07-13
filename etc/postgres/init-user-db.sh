669567e1c233ad131c2a99a6cf4f69220c9aeb11
soapmactavish23 <ricknogueira1231@gmail.com>
2026-07-13 16:48:32 -0300

    6.8. Preparando banco de dados para o Authorization Server

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE fastpay;
+	CREATE DATABASE fastpay;
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
+	CREATE DATABASE authorization_server;
 	CREATE DATABASE ordering_test;
-  CREATE DATABASE billing_test;
+	CREATE DATABASE billing_test;
+	CREATE DATABASE authorization_server_test;
 EOSQL
